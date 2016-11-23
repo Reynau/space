9b0c919f70a2f5f9858a72a6cd62381d4d5b06ed
Reynau <xavi.lopez.reynau@gmail.com>
2016-11-23 17:27:30 +0100

    Order and refactorize the code

--- space.js
+++ space.js
@@ -1,35 +1,27 @@
 var renderer = PIXI.autoDetectRenderer(1920, 1080, { antialias: true });
 renderer.view.style.width = "100%";
 renderer.view.style.height = "100%";
-
 document.body.appendChild(renderer.view);
+
 var stage = new PIXI.Container();
 
-// Render a circle to a texture
-var texture = new PIXI.RenderTexture(renderer, 16, 16);
-var graphics = new PIXI.Graphics();
-graphics.beginFill(0xFFFFFF);
-graphics.drawCircle(8, 8, 1);
-graphics.endFill();
-texture.render(graphics);
+// Init sprites
+var circle = createCircleSprite();
 
+// Constants
 var numStars = 1000;
+
+// Init stars
 var stars = [];
 for(var i=0; i<numStars; i++) {
-  var s = new PIXI.Sprite(texture);
-  var scale = Math.random() * 2;
-  s.position.x = Math.random() * renderer.width;
-  s.position.y = Math.random() * renderer.height;
-  s.alpha = Math.random();
-  s.scale.x = scale;
-  s.scale.y = scale;
+  var s = createStar();
   stars.push(s);
   stage.addChild(s);
 }
 renderer.render(stage);
 
+// Render loop
 requestAnimationFrame(animate);
-
 function animate() {
   requestAnimationFrame( animate );
   var time = Date.now() / 1000;
@@ -40,4 +32,27 @@ function animate() {
     s.alpha = freq * Math.sin(time + ampl);
   }
   renderer.render(stage);
+}
+
+// Sprite constructors
+function createCircleSprite () {
+  var circle = new PIXI.RenderTexture(renderer, 16, 16);
+  var graphics = new PIXI.Graphics();
+  graphics.beginFill(0xFFFFFF);
+  graphics.drawCircle(8, 8, 1);
+  graphics.endFill();
+  circle.render(graphics);
+  return circle;
+}
+
+// Element constructors
+function createStar () {
+  var s = new PIXI.Sprite(circle);
+  var scale = Math.random() * 2;
+  s.position.x = Math.random() * renderer.width;
+  s.position.y = Math.random() * renderer.height;
+  s.alpha = Math.random();
+  s.scale.x = scale;
+  s.scale.y = scale;
+  return s;
 }
